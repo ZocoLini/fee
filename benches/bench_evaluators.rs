51128b1dea4a30f4d1cc02dd29a5b6bb06a83144
Borja Castellano <borjacastellano1@gmail.com>
2025-08-29 10:18:11 +0000

    created benches for the evaluators

--- benches/bench_evaluators.rs
+++ benches/bench_evaluators.rs
@@ -1,1 +1,29 @@
+use std::hint::black_box;
 
+use criterion::{Criterion, criterion_group, criterion_main};
+use fee::{DefaultFnResolver, DefaultVarResolver, RPNEvaluator, prelude::*};
+
+fn rpn_evaluator(c: &mut Criterion)
+{
+    c.bench_function("eval/rpn", |b| {
+        let var_resolver = DefaultVarResolver::new();
+        let fn_resolver = DefaultFnResolver::new();
+
+        let expr = "(2 * 21) + 3 - 35 - ((5 * 80) + 5) + 10";
+        let context = Context::new(var_resolver, fn_resolver);
+        let evaluator = RPNEvaluator::new(expr, &context).unwrap();
+
+        b.iter(|| {
+            black_box(evaluator.eval());
+        });
+    });
+
+    c.bench_function("eval/rust_native", |b| {
+        b.iter(|| {
+            black_box((2 * 21) + 3 - 35 - ((5 * 80) + 5) + 10);
+        });
+    });
+}
+
+criterion_group!(benches, rpn_evaluator,);
+criterion_main!(benches);
