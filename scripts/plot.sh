2bee548404a026b5a2adc9ddfd3b690863bfdd7f
Borja Castellano <borjacastellano1@gmail.com>
2025-09-10 22:07:03 +0000

    bug fixed in the plot script

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ -d .venv ]; then
+if [ ! -d .venv ]; then
     python3 -m venv .venv
     source .venv/bin/activate
     pip3 install matplotlib
 fi
 
+source .venv/bin/activate
 python3 scripts/plot_benches.py
